096c6161c97e3dbaaa9c682ab33d425c093a764f
Ruwanya Hettiarachchi <ruwanyahettiarachchi@gmail.com>
2024-07-15 00:19:24 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,13 @@ import About from './pages/About';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+
+      <Header/>
 
       <Routes>
         <Route path='/' element= {<Home/>} />
